b01e21d1058fe3a25d3f347287174531938163a2
kevinbhuva369 <bhuvakevin108@gmail.com>
2025-08-25 12:51:14 +0530

    format doc

--- p2.c
+++ p2.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
- 
 
-int main(){
-     int num=0;
+int main()
+{
+   int num = 0;
 
-        loop:
-        printf("%d\n",num);
-        num++;
-     if (num!=90)
-     {
-            goto loop;
-        /* code */
-     }
-     
+loop:
+   printf("%d\n", num);
+   num++;
+   if (num != 90)
+   {
+      goto loop;
+      /* code */
+   }
 }
