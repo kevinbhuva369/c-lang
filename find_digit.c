17bdefd2c67539cc1f5abdb7448c9e7d2f3b182c
kevinbhuva369 <bhuvakevin108@gmail.com>
2025-08-27 12:16:21 +0530

    add file

---
+++ find_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int dijit, i = 1;
+    printf("ener dijit:");
+    scanf("%d", &dijit);
+
+    for (dijit != 0; dijit /= 10; i++)
+    {
+    }
+    printf("you enetr %d dijit numbar", i);
+}
