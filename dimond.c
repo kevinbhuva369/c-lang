4d18e9f6f54f5515c291a0dc9c6e4afcaf538902
kevinbhuva369 <bhuvakevin108@gmail.com>
2025-08-28 19:10:49 +0530

    add dimond parrten

---
+++ dimond.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("enetr number:");
+    scanf("%d", &num);
+
+    for (int i = 1; i <= num; i++)
+    {
+        for (int o = 1; o <= num - i; o++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 1; k <= i; k++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+    for (int i = num; i >= 0; i--)
+    {
+        for (int o = 1; o <= num - i; o++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 1; k <= i; k++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+}
