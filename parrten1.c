f0ce020cba1854970629e98b0d53104831870f8b
kevinbhuva369 <bhuvakevin108@gmail.com>
2025-09-02 12:45:24 +0530

    remove

---
+++ parrten1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("enter num ");
+    scanf("%d", &num);
+
+    for (int i = 1; i <= num; i++)
+    {
+        for (int k = 0; k < i; k++)
+        {
+            printf(" *");
+        }
+        printf("\n");
+    }
+    for (int y = num-1; y >0; y--)
+    {
+          for (int l = 0; l <y; l++)
+          {
+           printf(" *");
+          }
+          
+        printf("\n");
+    }
+    
+}
